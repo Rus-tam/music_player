1e334ed876f4ff5abe1420c48ef139acdb312cd0
Rustam <ahmetov.rustam2011@gmail.com>
2021-01-05 23:51:45 +0500

    initial_commit_with_some_JS

---
+++ src/index.js
@@ -0,0 +1,91 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
+const music = document.querySelector('audio')
+const prevBtn = document.querySelector('#prev')
+const playBtn = document.querySelector('#play')
+const nextBtn = document.querySelector('#next')
+
+//Music
+const songs = [
+    {
+        name: 'Jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'Jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'Jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (remix)',
+        artist: 'Metric/Jasinto Design'
+    }
+]
+
+//Check if Playing
+let isPlaying = false
+
+//Play
+const playSong = () => {
+    isPlaying = true
+    playBtn.classList.replace('fa-play', 'fa-pause')
+    playBtn.setAttribute('title', 'Pause')
+    music.play()
+}
+
+//Pause
+const pauseSong = () => {
+    isPlaying = false
+    playBtn.classList.replace('fa-pause', 'fa-play')
+    playBtn.setAttribute('title', 'Play')
+    music.pause()
+}
+
+//Play or Pause Event Listner
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+//Update DOM
+const loadSong = (song) => {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `./music/${song.name}.mp3`
+    image.src = `./img/${song.name}.jpg`
+}
+
+//Current Song
+let songIndex = 0
+
+//previous song
+const prevSong = () => {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+//next song
+const nextSong = () => {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex])
+
+//Event Listners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
