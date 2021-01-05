ae500e75d64752eb8c0154d9ca5acc06e99af84a
Rustam <ahmetov.rustam2011@gmail.com>
2021-01-06 01:05:27 +0500

    progress_bar_1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const prevBtn = document.querySelector('#prev')
 const playBtn = document.querySelector('#play')
 const nextBtn = document.querySelector('#next')
@@ -86,6 +88,17 @@ const nextSong = () => {
 // On Load - Select First Song
 loadSong(songs[songIndex])
 
+//Update Progress Bar and Time
+const updateProgressBar = (e) => {
+    if (isPlaying) {
+        const {duration, currentTime} = e.srcElement
+        //Update progress bar width
+        const progressPercent = (currentTime / duration) * 100
+        progress.style.width = `${progressPercent}%`
+    }
+}
+
 //Event Listners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)
